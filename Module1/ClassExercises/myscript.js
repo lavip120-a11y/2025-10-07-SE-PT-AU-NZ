92c30bf911c6769ddd9f0547035d9c622f8cd00d
lavip120-a11y <lavip120@gmail.com>
2025-10-16 17:38:45 +1300

    myscript and exercise3 update

--- Module1/ClassExercises/myscript.js
+++ Module1/ClassExercises/myscript.js
@@ -1,6 +1,9 @@
 function helloWorld() {
     document.getElementById("button1").style.color = "red"
 }
-function changecolumn() {
-    document.getElementById("button1").style.color = "red"
+function changeBackgroundColour() {
+    document.getElementById("column1").style.background = "blue"
+}
+function changeHeadingText() {
+    document.getElementById("column2").style.color = "pink"
 }

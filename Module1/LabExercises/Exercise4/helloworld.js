09a1afb06d16ef162ef9717f7ab9ba1fcfd703be
lavip120-a11y <lavip120@gmail.com>
2025-10-18 17:38:18 +1300

    Exercise4 completed

---
+++ Module1/LabExercises/Exercise4/helloworld.js
@@ -0,0 +1,25 @@
+function Sum(a, b){
+    console.log(a + b);
+    return a + b;
+}
+function Multiply(a, b){
+    return a * b;
+}
+function Subtract(a, b){
+    return a - b;
+}
+function Divide(a, b){
+    return a / b;
+}
+
+console.log(Sum(5, 1));
+console.log(Subtract(5, 1));
+console.log(Divide(5, 1));
+console.log(Multiply(Sum(2, 3), Sum(11, 5)));
+
+
+function greet(name) {
+    return "Hello" + name;
+}
+
+console.log(greet("John"));

371f5c8e818f06825351f946c7a184845834d889
lavip120-a11y <lavip120@gmail.com>
2025-11-09 17:23:59 +1300

    TypeofExample.js number boolean string object etc

---
+++ Module3/ClassExercises3/TypeofExample.js
@@ -0,0 +1,14 @@
+let result = 123; //calling the data 
+console.log(typeof result); // useful to make sure the data type in the function above is an object before proceeding
+
+console.log(typeof undefined); // undefined
+console.log(typeof 0); // number
+console.log(typeof 10n); // bigint
+console.log(typeof true); // boolean
+console.log(typeof 'text'); // string
+console.log(typeof Symbol('id')) // symbol
+console.log(typeof Math); // object
+console.log(typeof null); // object
+console.log(typeof console.log); // function
+
+console.log(typeof result);

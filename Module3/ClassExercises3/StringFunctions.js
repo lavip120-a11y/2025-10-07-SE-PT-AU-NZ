fc14cd8d8a76932d516e25ab5cde6dedc4eb3cae
lavip120-a11y <lavip120@gmail.com>
2025-11-19 16:58:09 +1300

    StringFunctions.js

--- Module3/ClassExercises3/StringFunctions.js
+++ Module3/ClassExercises3/StringFunctions.js
@@ -5,11 +5,10 @@ console.log(string.indexOf('anything')) // 11 - starting at 0 for 'I'
 console.log(string.substring(20)) // 'you like' - substring starting at position 20
 console.log(string.toUpperCase()) // I COULD BE ANYTHING YOU LIKE
 console.log(string + ' plus more') // I could be anything you like plus more
+console.log(string.lastIndexOf("anything")); //last occurrence of a string
 
 let string1 = "Hello this is a sample string";
 console.log(string1.length);
-
-console.log(string.lastIndexOf("anything")); //last occurrence of a string
 
 const sentence = 'The quick brown fox jumps over the lazy dog.';
 
@@ -19,4 +18,4 @@ console.log(sentence.split(' ')) // splits into multiple strings using the given
 // ['The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog.']
 console.log(sentence.slice(4, 10)) // quick - gets the section between chars 4 and 10
 console.log(sentence.replace('quick', 'slow')) // The slow brown fox jumps over the lazy dog.
-console.log(" extra spaces ".trim()) // extra spaces - trims whitespace from start and end
+console.log(" extra spaces ".trim()) // extra spaces - trims whitespace from start and end not the middle

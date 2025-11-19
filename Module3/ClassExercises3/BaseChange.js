a1fd147e6430395551a6ff924105780f48246476
lavip120-a11y <lavip120@gmail.com>
2025-11-19 17:04:57 +1300

    BaseChange.js

--- Module3/ClassExercises3/BaseChange.js
+++ Module3/ClassExercises3/BaseChange.js
@@ -1,8 +1,9 @@
 const ff = 255
 const ee = 238
 const dd = 221
-console.log(ff.toString(16)) // ff and turning it to hex
-console.log(ff.toString(8)) // taking ff and turning it to octal
+
+console.log(ff.toString(16)) // ff and turning it to hexadecimal format
+console.log(ff.toString(8)) // taking ff and turning it to octal representation
 
 //convert from rgb colour code to hexadecimal
 console.log(`#${ff.toString(16)}${ee.toString(16)}${dd.toString(16)}`) // #ffeedd

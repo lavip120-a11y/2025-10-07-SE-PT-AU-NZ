4a20329ce5e74f73308001b64ffb6e53ca7152c9
lavip120-a11y <lavip120@gmail.com>
2025-11-19 17:03:27 +1300

    hexNumbers.js

--- Module3/ClassExercises3/HexNumbers.js
+++ Module3/ClassExercises3/HexNumbers.js
@@ -2,5 +2,5 @@ const r = 0xff;
 const g = 0xff;
 const b = 0xff;
 
-const white = `rgb(${r}, ${g}, ${b})`
-console.log(white) // rgb(255, 255, 255)
+const white = `rgb(${r}, ${g}, ${b})` //hexadecimal representation
+console.log(white) // rgb(255, 255, 255) // converts the hexadecimal number into number for the colour

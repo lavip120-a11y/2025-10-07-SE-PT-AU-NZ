c6e890bc1e949176abf897d6f5bb4bf27c12a7ed
lavip120-a11y <lavip120@gmail.com>
2025-11-19 17:04:18 +1300

    BinaryOctal.js

--- Module3/ClassExercises3/BinaryOctal.js
+++ Module3/ClassExercises3/BinaryOctal.js
@@ -1,4 +1,4 @@
-const mobile = "041234567" //store a phone number as a string because you cannot store oh number with 0 to start
+const mobile = "041234567" //store a phone number as a string because you cannot store a number with 0 to start
 console.log(mobile) // 8730999 - decimal equivalent
 
 const binary = 0b11111111 // binary form of 255 (base 2)

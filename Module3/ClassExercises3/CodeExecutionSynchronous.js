2058a42612e642d2fb5c01e65bdb332949b2f5c1
lavip120-a11y <lavip120@gmail.com>
2025-11-20 14:05:02 +1300

    CodeExecutionSynchronous.js

---
+++ Module3/ClassExercises3/CodeExecutionSynchronous.js
@@ -0,0 +1,29 @@
+console.log("First");
+console.log("Second");
+
+setTimeout(() => console.log("Third"), 2000); //delayed code execution by 2 seconds
+//setTimeout will always give a slight delay and therefore those conse without will print first 
+
+// function printThirdLine() //you can add the function above ... 
+// {
+// console.log("Third"); //delay by 2 seconds
+// }
+
+console.log("Fourth");
+console.log("Fifth");
+console.log("Sixth");
+
+function Sum(a,b) {
+console.log(a+b);
+
+let timerID = setTimeout(Sum, 2000, 10, 20); //reference to timer
+
+for (let i= 0; i < 10; i++)
+if (i == 5) {
+    clearInterval(timerID);
+    console.log("Processing stopped..");
+    break; //breaks the loop
+}
+}
+
+clearTimeout(timerID); //clears or stops the execution of set timeout function - nothing will run.

39c257edd061fab9018b0c5941b9c00da4aaacab
lavip120-a11y <lavip120@gmail.com>
2025-11-20 13:57:48 +1300

    MapExample.js

--- Module3/ClassExercises3/MapExample2.js
+++ Module3/ClassExercises3/MapExample2.js
@@ -1,9 +1,9 @@
-let customer = { custName: "Smith", location: "Sydney" };
+let customer = { custName: "Smith", location: "Sydney" }; //customer object
 
-let order = { orderdescription: "Iphone 17", orderDate: "2025-05-17" };
+let order = { orderdescription: "Iphone 17", orderDate: "2025-05-17" }; //customer order
+//define a map
+let customerOrderMap = new Map(); // the 2 seperate objects above can be combined in 1 place by using map
 
-let customerOrderMap = new Map();
-
-customerOrderMap.set(customer, order);
-customerOrderMap.get(customer);
+customerOrderMap.set(customer, order); //joining together in 1 place
+customerOrderMap.get(customer); //what has customer ordered
 console.log(customerOrderMap.get(customer));
